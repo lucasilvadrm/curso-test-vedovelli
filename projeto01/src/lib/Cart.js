db615ad3af24f0dea596d9d48821f88137eb3714
Lucas Silva <lucas.silva@riachuelo.com.br>
2023-01-05 10:23:38 -0300

    feat: finalizando método summary

--- projeto01/src/lib/Cart.js
+++ projeto01/src/lib/Cart.js
@@ -16,10 +16,24 @@ export default class Cart {
     remove(this.items, { product });
   }
 
-  checkout() {
+  summary() {
+    const total = this.getTotal();
+    const items = this.items;
+
     return {
-      total: this.getTotal(),
-      items: this.items,
+      total,
+      items,
+    };
+  }
+
+  checkout() {
+    const { total, items } = this.summary();
+
+    this.items = [];
+
+    return {
+      total,
+      items,
     };
   }
 
